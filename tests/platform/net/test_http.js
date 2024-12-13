ff32e0b230c06e4acb457bf0a0b1807ec293fe78
gabriel-student-uefs <24115042@discente.uefs.br>
2024-12-13 20:02:18 -0300

    smell2

--- tests/platform/net/test_http.js
+++ tests/platform/net/test_http.js
@@ -15,7 +15,7 @@ describe('pc.Http', function () {
         sinon.restore();
     });
 
-    it('get() retries resource and returns result if eventually found', function (done) {
+    it('get() retries resource and returns result on third attempt', function (done) {
         sinon.spy(pc.http, 'request');
 
         var requests = 0;
@@ -23,7 +23,8 @@ describe('pc.Http', function () {
         xhr.onCreate = function (xhr) {
             setTimeout(function () {
                 try {
-                    if (++requests === 3) {
+                    requests++;
+                    if (requests === 3) {
                         xhr.respond(200, { ContentType: 'application/json' }, JSON.stringify({ test: "value" }));
                     } else {
                         xhr.error();
@@ -41,6 +42,32 @@ describe('pc.Http', function () {
             expect(err).to.equal(null);
             expect(pc.http.request.callCount).to.equal(3);
             expect(data).to.deep.equal({ test: "value" });
+            done();
+        });
+    });
+
+    it('get() retries resource and fails after max retries', function (done) {
+        sinon.spy(pc.http, 'request');
+
+        var requests = 0;
+        xhr = sinon.useFakeXMLHttpRequest();
+        xhr.onCreate = function (xhr) {
+            setTimeout(function () {
+                try {
+                    requests++;
+                    xhr.error();
+                } catch (err) {
+                    done(new Error(err.message + '\n' + err.stack));
+                }
+            });
+        };
+
+        pc.http.get('/someurl.json', {
+            retry: true,
+            maxRetries: 2
+        }, function (err, data) {
+            expect(err).to.not.equal(null);
+            expect(pc.http.request.callCount).to.equal(3);
             done();
         });
     });
